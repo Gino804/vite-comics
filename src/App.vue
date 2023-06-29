77e565ff399e8251c6b28eb0f92721ac7528fdca
Gino804 <giacomo.p03@gmail.com>
2023-06-29 17:53:51 +0200

    Added app header

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,45 @@
 <script>
+
+import AppHeader from './components/AppHeader.vue';
+
 export default {
-  data() {
-    [
-      {
-        text: 'Characters',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Comics',
-        url: '#',
-        current: true,
-      },
-      {
-        text: 'Movies',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'TV',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Games',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Collectibles',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Videos',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Fans',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'News',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Shop',
-        url: '#',
-        current: false,
-      },
-    ];
+  components: {
+    AppHeader
   }
 }
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+</template>
 
-<style></style>
+<style>
+/* CSS Reset */
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* Generics */
+
+body {
+  font-family: "Open Sans", sans-serif;
+}
+
+ul {
+  list-style-type: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+/* Utils */
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,117 @@
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ]
+        }
+    },
+}
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <nav>
+                <figure>
+                    <img src="../assets/img/dc-logo.png" alt="DC Logo">
+                </figure>
+                <ul>
+                    <li v-for="link in links" :key="link.text"><a :href="link.url" :class="{ 'active': link.current }">{{
+                        link.text }}</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+header {
+    height: 150px;
+    padding: 20px 0;
+}
+
+nav {
+    display: flex;
+    justify-content: space-between;
+}
+
+ul {
+    display: flex;
+}
+
+li {
+    margin: 0 20px;
+    height: 100%;
+}
+
+a {
+    text-transform: uppercase;
+    color: rgb(80, 78, 78);
+    font-size: 14px;
+    font-weight: bold;
+    height: 100%;
+    display: flex;
+    align-items: center;
+}
+
+li:hover a {
+    color: #0282F9;
+    border-bottom: 3px solid #0282F9;
+}
+
+.active {
+    color: #0282F9;
+    border-bottom: 3px solid #0282F9;
+}
+</style>
