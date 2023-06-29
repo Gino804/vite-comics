515ec678dc85715e4e169b422e036d11f3bf7637
Gino804 <giacomo.p03@gmail.com>
2023-06-29 18:47:55 +0200

    Added app main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,18 @@
+<template>
+    <main>
+        <section id="work-in-progress">
+            <div class="container">
+                <h2>Content goes here</h2>
+            </div>
+        </section>
+    </main>
+</template>
+
+<style scoped>
+#work-in-progress {
+    height: 150px;
+    line-height: 150px;
+    background-color: #1C1C1C;
+    color: white;
+}
+</style>
